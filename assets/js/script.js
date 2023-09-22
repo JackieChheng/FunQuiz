d920aa2cc6d4d931dc329f65e4acee4ecc975e8a
Jackie Chheng <jackiechheng@Jackies-MBP.T-mobile.com>
2023-09-21 18:32:43 -0600

    added timer when click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-var time = document.getElementById('time')
+var timerEl = document.getElementById('time');
+var startButton = document.querySelector('.btn');
+
+function countdown() {
+    var timeLeft = 60;
+    startButton.disabled = true; 
+
+    var timerInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            timerEl.textContent = 'Time Left: ' + timeLeft + 's';
+            timeLeft--;
+        } else {
+            timerEl.textContent = '';
+            clearInterval(timerInterval);
+            // displaymessage(); // You may want to call your function here if needed
+        }
+    }, 1000);
+}
+
+startButton.addEventListener('click', countdown); // Start the countdown when the button is clicked
