97eb5e4a2b975100992fa0612ca98285cd8bb1b6
Jackie Chheng <jackiechheng@Jackies-MacBook-Pro.local>
2023-09-25 13:41:03 -0600

    Added timer, buttons, and questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,59 @@
 var timerEl = document.getElementById('time');
-var startButton = document.querySelector('.btn');
+var startButton = document.getElementById('btn');
+var quizQuestions = document.getElementById('quizQuestions');
+var preQuiz = document.getElementById('preQuiz');
+var correctButton = document.getElementById('correct'); 
+var wrongButton1 = document.getElementById('wrong1'); 
+var wrongButton2 = document.getElementById('wrong2');
+var wrongButton3 = document.getElementById('wrong3');
+var failureMessage = document.getElementById('failureMessage');
+
+var timeLeft = 60;
+var timerInterval;
 
 function countdown() {
-    var timeLeft = 60;
-    startButton.disabled = true; 
+    startButton.disabled = true;
+    preQuiz.style.display = 'none';
+    quizQuestions.style.display = 'block';
 
-    var timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         if (timeLeft > 0) {
             timerEl.textContent = 'Time Left: ' + timeLeft + 's';
             timeLeft--;
         } else {
             timerEl.textContent = '';
             clearInterval(timerInterval);
-            // displaymessage(); // You may want to call your function here if needed
+            showFailureMessage(); // Show the failure message when time runs out
         }
     }, 1000);
 }
 
-startButton.addEventListener('click', countdown); // Start the countdown when the button is clicked
+function handleCorrectAnswer() {
+    quizQuestions.style.display = 'none';
+    // quizQuestions2.style.display = 'block';
+}
+
+function handleWrongAnswer() {
+    var currentTime = parseInt(timerEl.textContent.split(':')[1]); // Extract remaining seconds
+    currentTime -= 15; // Deduct 15 seconds
+
+    if (currentTime < 0) {
+        currentTime = 0; // Ensure the timer doesn't go negative
+    }
+
+    quizQuestions.style.display = 'none';
+
+    timerEl.textContent = 'Time Left: ' + currentTime + 's';
+}
+
+function showFailureMessage() {
+    failureMessage.style.display = 'block'; // Display the failure message
+}
+
+startButton.addEventListener('click', countdown);
+
+correctButton.addEventListener('click', handleCorrectAnswer); // Handle correct answer
+
+wrongButton1.addEventListener('click', handleWrongAnswer);
+wrongButton2.addEventListener('click', handleWrongAnswer);
+wrongButton3.addEventListener('click', handleWrongAnswer);
